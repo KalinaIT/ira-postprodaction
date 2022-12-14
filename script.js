5491d0841ef97e61d364c807fafa6077cfb54a30
KalinaIT <kalinatwich@gmail.com>
2022-12-14 03:01:31 +0200

    slider,js open img

--- script.js
+++ script.js
@@ -2,23 +2,19 @@
 let modal = document.querySelector("#myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.querySelector("#myImg");
+let img = document.querySelectorAll("#myImg");
 let modalImg = document.querySelector("#img01");
 let captionText = document.querySelector("#caption");
-img.addEventListener('click' , display)
-function display(){
+img.forEach(item => item.addEventListener('click' , showModalPhoto)
+ )
+ function showModalPhoto(){
    modal.style.display = "block";
    modalImg.src = this.src;
    captionText.innerHTML = this.alt;
  }
 
-// Get the <span> element that closes the modal
-let span = document.querySelector(".close");
-
 // When the user clicks on <span> (x), close the modal
-modal.onclick = function(event) {
-   if(event.currentTarget =! modalImg){
+modal.onclick = function() {
   modal.style.display = "none";
-   }
 }
 
