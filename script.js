2f871a536a7495949b6b169232af26330ee72d06
KalinaIT <kalinatwich@gmail.com>
2022-12-15 23:19:21 +0200

    add about

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let modal = document.querySelector("#myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.querySelectorAll("#myImg");
+let img = document.querySelectorAll(".myImg");
 let modalImg = document.querySelector("#img01");
 let captionText = document.querySelector("#caption");
 img.forEach(item => item.addEventListener('click' , showModalPhoto)
